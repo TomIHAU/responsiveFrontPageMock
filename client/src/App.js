e8d3ad95364d41314d8632c7a53948a896861472
TauH <thaywood988@gmail.com>
2022-03-31 12:24:46 +1100

    some text done

--- client/src/App.js
+++ client/src/App.js
@@ -7,12 +7,33 @@ function App() {
     <div className="App">
       <NavBar />
       <div className="BannerCar">
-        <img src="Q Imaging Mock Up-10.png" alt="Main Banner"></img>
+        <img src="./images/Q Imaging Mock Up-10.png" alt="Main Banner"></img>
       </div>
       <div className="bigLinks">
-        <img src="" alt="become a reseller"></img>
-        <img src="" alt="what's new"></img>
+        <div>
+          <img
+            src="./images/Q Imaging Mock Up-11.png"
+            alt="become a reseller"
+          ></img>
+          <img src="./images/Q Imaging Mock Up-12.png" alt="what's new"></img>
+        </div>
       </div>
+      <div className="individualLinks">
+        <div></div>
+      </div>
+      <img src="./images/Q Imaging Mock Up-13.png" alt="Our Brands"></img>
+      <div className="contactLinks"></div>
+      <footer>
+        <div>
+          <h4>Q-IMAGING AUSTRALIA</h4>
+          <p>Powered by TOTECS</p>
+        </div>
+        <div>
+          <p>Privacy Policy</p>
+          <p>Terms and conditions</p>
+          <p>Freight and Returns</p>
+        </div>
+      </footer>
     </div>
   );
 }
---
+++ client/src/components/FrontPageProduct.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+export default function NavBar() {
+  return (
+    <div className="product">
+      <div className="productInfo">
+        <h3>Q IMAGING PRODUCT ITEM - 1KG 123456789</h3>
+        <p>
+          <strong>Product Code: </strong>123456789
+        </p>
+        <p>Q IMAGING PRODUCT ITEM - 1KG 123456789</p>
+        <p>In Stock (15+)</p>
+      </div>
+      <button>REGISTER OR LOGIN</button>
+    </div>
+  );
+}
